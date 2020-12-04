8976501747f951620d01319946471cc12704bc7d
totallynotsmurf1 <75482876+totallynotsmurf1@users.noreply.github.com>
2020-12-04 13:26:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,236 @@
+var randomNumGen;
+
+
+var csgoQuotes = [];
+
+function quoteGen() {
+  var randomNumGen = Math.floor(Math.random() * 101);
+  switch (randomNumGen) {
+    case 0: document.getElementById('quotes').innerHTML = '❝ we have 2 child that they parent bulling them ❞ <br><br> -CeCiDiT AnGeLuS'
+    break;
+    case 1: document.getElementById('quotes').innerHTML = '❝ i wish to die all you family guys like you destroy the game ❞ <br><br> -CeCiDiT AnGeLuS'
+    break;
+    case 2: document.getElementById('quotes').innerHTML = '❝ maybe lem and u means u are goods ❞ <br><br> -LewSonHere'
+    break;
+    case 3: document.getElementById('quotes').innerHTML = '❝ u are creazy lems and u means u are smurfs ❞ <br><br> -LewSonHere'
+    break;
+    case 4: document.getElementById('quotes').innerHTML = '❝ THAT TROLLS IN MATCHMAKING ❞ <br><br> -STACHU???'
+    break;
+    case 5: document.getElementById('quotes').innerHTML = '❝ aha nice prefair ❞ <br><br> -Ботан TV'
+    break;
+    case 6: document.getElementById('quotes').innerHTML = '❝ Vac ban - it\'s don\'t anti-cheat. ❞ <br><br> -paradiseWherei'
+    break;
+    case 7: document.getElementById('quotes').innerHTML = '❝ report for abusive textchat ❞ <br><br> -▷hanskд◁'
+    break;
+    case 8: document.getElementById('quotes').innerHTML = '❝ Only had shot ❞ <br><br> -paradiseWherei'
+    break;
+    case 9: document.getElementById('quotes').innerHTML = '❝ kiko me ❞ <br><br> -I like cappuccino'
+    break;
+    case 10: document.getElementById('quotes').innerHTML = '❝ sorry gase ❞ <br><br> -карэбола'
+    break;
+    case 11: document.getElementById('quotes').innerHTML = '❝ Gays.pla say info. ❞ <br><br> -карэбола'
+    break;
+    case 12: document.getElementById('quotes').innerHTML = '❝ NICE TEA< ❞ <br><br> -карэбола'
+    break;
+    case 13: document.getElementById('quotes').innerHTML = '❝ nice reflect ❞ <br><br> -карэбола'
+    break;
+    case 14: document.getElementById('quotes').innerHTML = '❝ you u fuckin idiot ;P ❞ <br><br> -Madafaka'
+    break;
+    case 15: document.getElementById('quotes').innerHTML = '❝ first game not warm either ❞ <br><br> -Bad profile'
+    break;
+    case 16: document.getElementById('quotes').innerHTML = '❝ idot ❞ <br><br> -BARTO'
+    break;
+    case 17: document.getElementById('quotes').innerHTML = '❝ fuck all family ❞ <br><br> -ZamaZingo'
+    break;
+    case 18: document.getElementById('quotes').innerHTML = '❝ fuckyou teahcer ❞ <br><br> -ZamaZingo'
+    break;
+    case 19: document.getElementById('quotes').innerHTML = '❝ I LİKE RUSSİA GIRL, BUT FUCK FAMİLY ❞ <br><br> -ZamaZingo'
+    break;
+    case 20: document.getElementById('quotes').innerHTML = '❝ nice bh .... ❞ <br><br> -MyWay'
+    break;
+    case 21: document.getElementById('quotes').innerHTML = '❝ yellow runbust ? ❞ <br><br> -NexMoon'
+    break;
+    case 22: document.getElementById('quotes').innerHTML = '❝ we have not chance they have shox and sceam ❞ <br><br> -CheesyMcMegges'
+    break;
+    case 23: document.getElementById('quotes').innerHTML = '❝ What a fuck!?! ❞ <br><br> -shox'
+    break;
+    case 24: document.getElementById('quotes').innerHTML = '❝ dont big smoke ❞ <br><br> -shox'
+    break;
+    case 25: document.getElementById('quotes').innerHTML = '❝ boib b ❞ <br><br> -shox'
+    break;
+    case 26: document.getElementById('quotes').innerHTML = '❝ Good smoke grieffing ❞ <br><br> -shox'
+    break;
+    case 27: document.getElementById('quotes').innerHTML = '❝ see your map you will see red pint, twice? ❞ <br><br> -Scooper'
+    break;
+    case 28: document.getElementById('quotes').innerHTML = '❝ where all hear you x) ❞ <br><br> -Tinkiwinki'
+    break;
+    case 29: document.getElementById('quotes').innerHTML = '❝ you step like a tank ❞ <br><br> -✪lKrolNos'
+    break;
+    case 30: document.getElementById('quotes').innerHTML = '❝ you are fuck citer ❞ <br><br> -✪SPEKTO✪'
+    break;
+    case 31: document.getElementById('quotes').innerHTML = '❝ did you love isis ❞ <br><br> -Rasha Biceps ツ'
+    break;
+    case 32: document.getElementById('quotes').innerHTML = '❝ retasrds ❞ <br><br> -Rasha Biceps ツ'
+    break;
+    case 33: document.getElementById('quotes').innerHTML = '❝ my team is ALL BOOT ❞ <br><br> -Beast #LEGEND'
+    break;
+    case 34: document.getElementById('quotes').innerHTML = '❝ fuck you mum ❞ <br><br> -А Л Ё Ш А'
+    break;
+    case 35: document.getElementById('quotes').innerHTML = '❝ i have more than 1 brain that you write ARE ❞ <br><br> -Smartyy'
+    break;
+    case 36: document.getElementById('quotes').innerHTML = '❝ only i go apartmans ❞ <br><br> -ferik35 RoadToMG'
+    break;
+    case 37: document.getElementById('quotes').innerHTML = '❝ nice team reprot ❞ <br><br> -SilenthDeath'
+    break;
+    case 38: document.getElementById('quotes').innerHTML = '❝ you team reprot ❞ <br><br> -SilenthDeath'
+    break;
+    case 39: document.getElementById('quotes').innerHTML = '❝ big smoke can see everyone, report ❞ <br><br> -AKBABA'
+    break;
+    case 40: document.getElementById('quotes').innerHTML = '❝ u shoot my head when i jump drag shot like aim lock ❞ <br><br> -Kojala#THUGLIFE'
+    break;
+    case 41: document.getElementById('quotes').innerHTML = '❝ GO TO THE BOOM ❞ <br><br> -my name is jeff'
+    break;
+    case 42: document.getElementById('quotes').innerHTML = '❝ thanks but this don\'t win match ❞ <br><br> -русский'
+    break;
+    case 43: document.getElementById('quotes').innerHTML = '❝ nice noob p90 camp on TT spawn silver elite ❞ <br><br> -KruKu'
+    break;
+    case 44: document.getElementById('quotes').innerHTML = '❝ why he tries knife, just losed the won round ❞ <br><br> -Pablurp'
+    break;
+    case 45: document.getElementById('quotes').innerHTML = '❝ PLAY CLEAR ❞ <br><br> -Bestoff SGT'
+    break;
+    case 46: document.getElementById('quotes').innerHTML = '❝ HAHA NEGAV ❞ <br><br> -AqùaM3rce❚ary'
+    break;
+    case 47: document.getElementById('quotes').innerHTML = '❝ you got a reker ❞ <br><br> -✪Izumi'
+    break;
+    case 48: document.getElementById('quotes').innerHTML = '❝ the homur is in the air ❞ <br><br> -ManFo'
+    break;
+    case 49: document.getElementById('quotes').innerHTML = '❝ nehgeihv ❞ <br><br> -✪Izumi'
+    break;
+    case 50: document.getElementById('quotes').innerHTML = '❝ everyone corner ❞ <br><br> -✪Izumi'
+    break;
+    case 51: document.getElementById('quotes').innerHTML = '❝ do you have fck monitor or autyzm ❞ <br><br> -JAKAŚ SUKA SIE JEBAĆ'
+    break;
+    case 52: document.getElementById('quotes').innerHTML = '❝ i dont have rece, i play nogami ❞ <br><br> -JAKAŚ SUKA SIE JEBAĆ'
+    break;
+    case 53: document.getElementById('quotes').innerHTML = '❝ how many bulec has you ❞ <br><br> -✪GonniPro✪'
+    break;
+    case 54: document.getElementById('quotes').innerHTML = '❝ nice degle ❞ <br><br> -TipiK◥◣ ◢◤'
+    break;
+    case 55: document.getElementById('quotes').innerHTML = '❝ go play blue whale you fat piece of weed shit ❞ <br><br> -dusan.sousek17 hellcase.com'
+    break;
+    case 56: document.getElementById('quotes').innerHTML = '❝ this map is so weird, and i am from sweden ❞ <br><br> -shdw'
+    break;
+    case 57: document.getElementById('quotes').innerHTML = '❝ I go to play DOTA 2 ❞ <br><br> -//'
+    break;
+    case 58: document.getElementById('quotes').innerHTML = '❝ hahaaahah 1vs4 you are bad gous ❞ <br><br> -Arre=DD'
+    break;
+    case 59: document.getElementById('quotes').innerHTML = '❝ wtf is to fast omg ❞ <br><br> -The HacKer BETS.gg'
+    break;
+    case 60: document.getElementById('quotes').innerHTML = '❝ I dont hacking ❞ <br><br> -Orbii'
+    break;
+    case 61: document.getElementById('quotes').innerHTML = '❝ smeke wh ❞ <br><br> -TROLL#FACE'
+    break;
+    case 62: document.getElementById('quotes').innerHTML = '❝ good jhob t3am ❞ <br><br> -koksal baba da k1ng'
+    break;
+    case 63: document.getElementById('quotes').innerHTML = '❝ only digel ❞ <br><br> -Delta Force USA Army'
+    break;
+    case 64: document.getElementById('quotes').innerHTML = '❝ dont eveen try to report me valulve dosnt give a dam shit ❞ <br><br> -Delta Force USA Army'
+    break;
+    case 65: document.getElementById('quotes').innerHTML = '❝ my team haclers XD ❞ <br><br> -Chr1sSss'
+    break;
+    case 66: document.getElementById('quotes').innerHTML = '❝ maincraft noob? hahahhahahah gramatical ❞ <br><br> -EmYL'
+    break;
+    case 67: document.getElementById('quotes').innerHTML = '❝ idiot tema ❞ <br><br> -VetoryQ_ hellcase.com'
+    break;
+    case 68: document.getElementById('quotes').innerHTML = '❝ team of idiot and shelder important idioit ❞ <br><br> -$ieg Kla$ (CSGOMIX.com)'
+    break;
+    case 69: document.getElementById('quotes').innerHTML = '❝ in m spra ❞ <br><br> -AWPtimus Prime'
+    break;
+    case 70: document.getElementById('quotes').innerHTML = '❝ chaterr ❞ <br><br> -GalaxY'
+    break;
+    case 71: document.getElementById('quotes').innerHTML = '❝ yes bye bye chiter xD ❞ <br><br> -Mota22'
+    break;
+    case 72: document.getElementById('quotes').innerHTML = '❝ 500 hours of game in this game ❞ <br><br> -кяуuм'
+    break;
+    case 73: document.getElementById('quotes').innerHTML = '❝ oto noob ❞ <br><br> -Sheikh Brando'
+    break;
+    case 74: document.getElementById('quotes').innerHTML = '❝ try get above me at the scorebord fgt ❞ <br><br> -AkimboFTW hellcase.com'
+    break;
+    case 75: document.getElementById('quotes').innerHTML = '❝ squaker ❞ <br><br> -21 SAVAGE'
+    break;
+    case 76: document.getElementById('quotes').innerHTML = '❝ smurf off??? ❞ <br><br> -BENFICA | CSGO500'
+    break;
+    case 77: document.getElementById('quotes').innerHTML = '❝ gloabal .. etc ❞ <br><br> -BENFICA | CSGO500'
+    break;
+    case 78: document.getElementById('quotes').innerHTML = '❝ im gonna find ur adress, trust me ....everything is possible ❞ <br><br> -☢ҭαgαOҭsiҭαѷ™☢'
+    break;
+    case 79: document.getElementById('quotes').innerHTML = '❝ nice kemp ❞ <br><br> -VACVACVACVAC'
+    break;
+    case 80: document.getElementById('quotes').innerHTML = '❝ flash and smash ❞ <br><br> -Fickoo'
+    break;
+    case 81: document.getElementById('quotes').innerHTML = '❝ i say ur mother its mu slut ❞ <br><br> -13 cm w zwisie DobryJackpot.pl'
+    break;
+    case 82: document.getElementById('quotes').innerHTML = '❝ but ur mom its cheating ❞ <br><br> -13 cm w zwisie DobryJackpot.pl'
+    break;
+    case 83: document.getElementById('quotes').innerHTML = '❝ u are reproted for being animal ❞ <br><br> -13 cm w zwisie DobryJackpot.pl'
+    break;
+    case 84: document.getElementById('quotes').innerHTML = '❝ you know what mute mea? ❞ <br><br> -21 Balion'
+    break;
+    case 85: document.getElementById('quotes').innerHTML = '❝ stfu u dump bitch ❞ <br><br> -Holloway'
+    break;
+    case 86: document.getElementById('quotes').innerHTML = '❝ kys grand ma ❞ <br><br> -kkk will find u'
+    break;
+    case 87: document.getElementById('quotes').innerHTML = '❝ i got legit just 1tappe ❞ <br><br> -Grandma couch'
+    break;
+    case 88: document.getElementById('quotes').innerHTML = '❝ How come you retard think someone was trying to be cocky? ❞ <br><br> -Kropaczka'
+    break;
+    case 89: document.getElementById('quotes').innerHTML = '❝ unfair black gypsy without life ❞ <br><br> -DONTFANTASTICKYPESS'
+    break;
+    case 90: document.getElementById('quotes').innerHTML = '❝ stfu nigger bondman ❞ <br><br> -DONTFANTASTICKYPESS'
+    break;
+    case 91: document.getElementById('quotes').innerHTML = '❝ black nigger i hope u die bondman cheapest nigger for americans :) u are black monkey kys nolifer ❞ <br><br> -DONTFANTASTICKYPESS'
+    break;
+    case 92: document.getElementById('quotes').innerHTML = '❝ bondman u dont have life but nobody cares cuz u are lower than normal white people :) ❞ <br><br> -DONTFANTASTICKYPESS'
+    break;
+    case 93: document.getElementById('quotes').innerHTML = '❝ niggers for die :() make world better ❞ <br><br> -DONTFANTASTICKYPESS'
+    break;
+    case 94: document.getElementById('quotes').innerHTML = '❝ ahahahah man tht was tough with the usb ❞ <br><br> -turtle'
+    break;
+    case 95: document.getElementById('quotes').innerHTML = '❝ when my reflect is faster than enemy body ❞ <br><br> -Fips13 CSGODouble.gg'
+    break;
+    case 96: document.getElementById('quotes').innerHTML = '❝ NIECE 100% ❞ <br><br> -Victoria/I\'m girl'
+    break;
+    case 97: document.getElementById('quotes').innerHTML = '❝ again 1 single no this enaught ❞ <br><br> -Bhawp 1'
+    break;
+    case 98: document.getElementById('quotes').innerHTML = '❝ i fucikng reportere you ❞ <br><br> -Kloppi'
+    break;
+    case 99: document.getElementById('quotes').innerHTML = '❝ all see all knew ❞ <br><br> -William Shakespeare'
+    break;
+    case 100: document.getElementById('quotes').innerHTML = '❝ its old cat. meeeeeeeeeeoooooooooooooow ❞ <br><br> -Old cat'
+    break;
+    case 101: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 102: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 103: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 104: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 105: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 106: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 107: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 108: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 109: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+    case 110: document.getElementById('quotes').innerHTML = '❝  ❞'
+    break;
+
+
+
+  }
+
+}
